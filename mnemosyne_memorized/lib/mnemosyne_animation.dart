9532e08b308a0f0759957d3544cc78ea1630704b
AsherHoltham <asher.holtham.github@gmail.com>
2025-05-05 23:06:23 -0700

    scales are all correct, implementing lerp accross x axis for graph animation

--- mnemosyne_memorized/lib/mnemosyne_animation.dart
+++ mnemosyne_memorized/lib/mnemosyne_animation.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'dart:math' as math;
+import 'dart:math' as math;
 
 const List<Color> networkGraphColors = [
   //way of depicting geyscale // input on gradient from white to this
@@ -41,6 +41,8 @@ class MnemosynePainter extends CustomPainter {
   final double time;
   final List<List<double>> data;
   List<BrainNode> nodeMap = [];
+  bool runAnimation = true;
+  bool animationFinished = false;
 
   MnemosynePainter({required this.time, required this.data});
 
@@ -76,10 +78,10 @@ class MnemosynePainter extends CustomPainter {
         double nodeValue = scaledCurrLayerValues[node];
         if (nodeValue > scaleCutOff) {
           double xPos =
-              (size.width * .025) + ((size.width * .95) / 4.5) * layer;
+              (size.width * .025) + ((size.width * .95) / 4.05) * layer;
           double yPos =
-              (size.height * .025) +
-              ((size.height * .95) / scaledCurrLayerValues.length) * node;
+              (size.height * .05) +
+              ((size.height * .90) / scaledCurrLayerValues.length) * node;
           double nodeRadius = (size.height * .005);
           Offset nodePos = Offset(xPos, yPos);
           List<BrainEdge> nodeEdges = [];
@@ -90,11 +92,10 @@ class MnemosynePainter extends CustomPainter {
           ) {
             if (scaledNextLayerValues[outNode] > scaleCutOff) {
               double xNextPos =
-                  (size.width * .025) + ((size.width * .95) / 4.5) * nextLayer;
+                  (size.width * .025) + ((size.width * .95) / 4.05) * nextLayer;
               double yNextPos =
-                  (size.height * .025) +
-                  ((size.height * .95) / scaledNextLayerValues.length) *
-                      outNode;
+                  (size.height * .05) +
+                  ((size.height * .9) / scaledNextLayerValues.length) * outNode;
               Offset nextPos = Offset(xNextPos, yNextPos);
               double edgeValue =
                   (scaledNextLayerValues[outNode] + nodeValue) / 2;
@@ -118,9 +119,9 @@ class MnemosynePainter extends CustomPainter {
       }
     }
     for (int outputs = 0; outputs < 10; outputs++) {
-      double xPos = (size.width * .025) + ((size.width * .95) / 4.5) * 4;
-      double yPos = (size.height * .025) + ((size.height * .95) / 10) * outputs;
-      double nodeRadius = ((size.height * .95) / 10) / 3;
+      double xPos = (size.width * .025) + ((size.width * .95) / 4.05) * 4;
+      double yPos = (size.height * .05) + ((size.height * .9) / 10) * outputs;
+      double nodeRadius = ((size.height * .95) / 10) / 2.5;
       Offset nodePos = Offset(xPos, yPos);
       BrainNode outNode = BrainNode(
         pos: nodePos,
@@ -131,25 +132,108 @@ class MnemosynePainter extends CustomPainter {
       );
       brainMap.add(outNode);
     }
-    //print(brainMap.length);
     return brainMap;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(Offset(0, 0), 10, Paint()..color = Colors.black);
-    canvas.drawCircle(
-      Offset(0, size.height),
-      10,
-      Paint()..color = Colors.black,
-    );
-    canvas.drawCircle(Offset(size.width, 0), 10, Paint()..color = Colors.black);
-    canvas.drawCircle(
-      Offset(size.width, size.height),
-      10,
-      Paint()..color = Colors.black,
-    );
     nodeMap = initNodeMap(data, size);
+
+    if (time < .5) {
+      _drawInitial(canvas, size);
+    } else if (runAnimation) {
+      double mvPct = time - .5;
+      if (mvPct > 1 && runAnimation) {
+        mvPct = 1;
+        runAnimation = false;
+      }
+      _drawMove(canvas, size, mvPct);
+    }
+    if (!runAnimation) {
+      _drawFullGraph(canvas, size, nodeMap);
+      animationFinished = true;
+    }
+  }
+
+  void _drawInitial(canvas, size) {
+    double blockDim = (size.height * .6) / 28;
+    double yOffset = size.height * .2;
+    double xOffset = (size.width - (size.height * .6)) / 2;
+
+    for (int i = 0; i < data[0].length; i++) {
+      int row = i ~/ 28;
+      int col = i % 28;
+      double xPos = col * blockDim + xOffset;
+      double yPos = row * blockDim + yOffset;
+
+      int alpha = ((data[0][i] * 255).round()).clamp(0, 255);
+      Color base = networkGraphColors[0];
+      final mPaint = Paint()..color = base.withAlpha(alpha);
+      Rect rect = Offset(xPos, yPos) & Size(blockDim, blockDim);
+      canvas.drawRect(rect, mPaint);
+    }
+  }
+
+  void _drawMove(canvas, size, mvPct) {
+    double yOffset = size.height * .2;
+    double xOffset = (size.width - (size.height * .6)) / 2;
+    int index = 0;
+    for (int i = 0; i < data[index].length; i++) {
+      double blockDim = (size.height * .6) / math.max(28, 28 * mvPct * 2);
+      int row = i ~/ 28;
+      int col = i % 28;
+      double xPos = col * blockDim + xOffset;
+      double yPos = row * blockDim + yOffset;
+      double xEndPos = (size.width * .025) + ((size.width * .95) / 4.5) * index;
+      double yEndPos =
+          (size.height * .05) + ((size.height * .90) / data[index].length) * i;
+      Offset start = Offset(xPos, yPos);
+      Offset end = Offset(xEndPos, yEndPos);
+      Offset curr = Offset.lerp(start, end, mvPct) ?? start;
+      int alpha = ((data[index][i] * 255).round()).clamp(0, 255);
+      Color base = networkGraphColors[index];
+      final mPaint = Paint()..color = base.withAlpha(alpha);
+      if (alpha > .5) {
+        if (mvPct < .95) {
+          final rect = Rect.fromLTWH(curr.dx, curr.dy, blockDim, blockDim);
+          final double radius = mvPct * 4;
+          final rrect = RRect.fromRectAndRadius(rect, Radius.circular(radius));
+          canvas.drawRRect(rrect, mPaint);
+        } else {
+          canvas.drawCircle(end, (size.height * .005), mPaint);
+        }
+      }
+    }
+  }
+
+  // void _drawAccrossGraph(canvas, size, nodeMap, percent) {
+  //   //double currXValue =
+  //   for (int i = 0; i < nodeMap.length; i++) {
+  //     int alpha = (nodeMap[i].scaledValue * 255).round().clamp(0, 255);
+  //     Color base = networkGraphColors[nodeMap[i].index];
+  //     final mPaint = Paint()..color = base.withAlpha(alpha);
+  //     canvas.drawCircle(nodeMap[i].pos, nodeMap[i].radius, mPaint);
+  //     for (int j = 0; j < nodeMap[i].outEdgeList.length; j++) {
+  //       int lineAlpha = (nodeMap[i].outEdgeList[j].scaledValue * 255)
+  //           .round()
+  //           .clamp(0, 255);
+  //       final linePaint =
+  //           Paint()
+  //             ..style = PaintingStyle.stroke
+  //             ..strokeWidth = .5
+  //             ..color = networkGraphColors[nodeMap[i].index].withAlpha(
+  //               lineAlpha,
+  //             );
+  //       canvas.drawLine(
+  //         nodeMap[i].outEdgeList[j].startPos,
+  //         nodeMap[i].outEdgeList[j].endPos,
+  //         linePaint,
+  //       );
+  //     }
+  //   }
+  // }
+
+  void _drawFullGraph(canvas, size, nodeMap) {
     for (int i = 0; i < nodeMap.length; i++) {
       int alpha = (nodeMap[i].scaledValue * 255).round().clamp(0, 255);
       Color base = networkGraphColors[nodeMap[i].index];
@@ -176,75 +260,7 @@ class MnemosynePainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(MnemosynePainter old) => false;
+  bool shouldRepaint(MnemosynePainter old) {
+    return old.time != time || old.data != data;
+  }
 }
-
-// void _drawInitial(canvas, size, initPct) {
-  //   double blockDim = (size.height * .6) / 28;
-  //   double yOffset = size.height * .2;
-  //   double xOffset = (size.width - (size.height * .6)) / 2;
-
-  //   for (int i = 0; i < data[0].length; i++) {
-  //     int row = i ~/ 28;
-  //     int col = i % 28;
-  //     double xPos = col * blockDim + xOffset;
-  //     double yPos = row * blockDim + yOffset;
-
-  //     int alpha = ((data[0][i] * 255).round()).clamp(0, 255);
-  //     Color base = networkGraphColors[0];
-  //     final mPaint = Paint()..color = base.withAlpha(alpha);
-  //     Rect rect = Offset(xPos, yPos) & Size(blockDim, blockDim);
-  //     canvas.drawRect(rect, mPaint);
-  //   }
-  // }
-
-  // void _drawMove(canvas, size, mvPct) {
-  //   double yOffset = size.height * .2;
-  //   double xOffset = (size.width - (size.height * .6)) / 2;
-  //   int index = 0;
-  //   for (int i = 0; i < data[index].length; i++) {
-  //     double blockDim = (size.height * .6) / math.max(28, 28 * mvPct * 2);
-  //     int row = i ~/ 28;
-  //     int col = i % 28;
-  //     double xPos = col * blockDim + xOffset;
-  //     double yPos = row * blockDim + yOffset;
-  //     double xEndPos = (size.width / 10) + (size.width / 5) * index;
-  //     double yEndPos = ((size.height * .95) / data[index].length) * i;
-  //     Offset start = Offset(xPos, yPos);
-  //     Offset end = Offset(xEndPos, yEndPos);
-  //     Offset curr = Offset.lerp(start, end, mvPct) ?? start;
-  //     int alpha = ((data[index][i] * 255).round()).clamp(0, 255);
-  //     Color base = networkGraphColors[index];
-  //     final mPaint = Paint()..color = base.withAlpha(alpha);
-  //     final rect = Rect.fromLTWH(curr.dx, curr.dy, blockDim, blockDim);
-  //     final double radius = mvPct * 4;
-  //     final rrect = RRect.fromRectAndRadius(rect, Radius.circular(radius));
-  //     canvas.drawRRect(rrect, mPaint);
-  //   }
-  // }
-
-  // void _drawL1(canvas, size, l1Pct) {
-  //   int index = 0;
-  //   double initNodeDim = (size.height * .6) / 112;
-  //   for (int i = 0; i < data[index].length; i++) {
-  //     double xPos = (size.width / 10) + (size.width / 5) * index;
-  //     double yPos = ((size.height * .95) / data[index].length) * i;
-  //     Offset nodePos = Offset(xPos, yPos);
-  //     int alpha = ((data[index][i] * 255).round()).clamp(0, 255);
-  //     Color base = networkGraphColors[index];
-  //     final mPaint = Paint()..color = base.withAlpha(alpha);
-  //     final linePaint =
-  //         Paint()
-  //           ..style = PaintingStyle.stroke
-  //           ..strokeWidth = 1.5
-  //           ..color = networkGraphColors[index].withAlpha(alpha);
-  //     canvas.drawCircle(nodePos, initNodeDim, mPaint);
-  //     for (int j = 0; j < data[1].length; j++) {
-  //       double xEndPos = (size.width / 10) + (size.width / 5);
-  //       double yEndPos = (((size.height * .95) / data[1].length) * j);
-  //       Offset end = Offset(xEndPos, yEndPos);
-  //       Offset curr = Offset.lerp(nodePos, end, l1Pct) ?? nodePos;
-  //       canvas.drawLine(nodePos, curr, linePaint);
-  //     }
-  //   }
-  // }
